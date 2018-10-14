3d92e41ad61d4c2734bf4b238c24e68cf50fdaa8
Caroline <csryan418@gmail.com>
2018-10-14 19:36:48 -0400

    blocjams-sliders_assignment: completed

---
+++ scripts/helper.js
@@ -0,0 +1,9 @@
+class Helper {
+  playPauseAndUpdate(song) {
+    const duration = player.getDuration();
+    $('#time-control .total-time').text ( player.prettyTime(duration) );
+    player.playPause(song);
+  };
+}
+
+const helper = new Helper();
